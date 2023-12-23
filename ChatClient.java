a710f95fd2004272a96439278d8584bb6e1a95e5
Miguel Ferreira <miguelsf1@sapo.pt>
2023-12-23 01:03:29 +0000

    progress 5

--- ChatClient.java
+++ ChatClient.java
@@ -3,23 +3,14 @@ import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
 import java.nio.charset.*;
-import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
 public class ChatClient {
-
-    // Variáveis relacionadas com a interface gráfica --- * NÃO MODIFICAR *
     JFrame frame = new JFrame("Chat Client");
     private JTextField chatBox = new JTextField();
     private JTextArea chatArea = new JTextArea();
-    // --- Fim das variáveis relacionadas coma interface gráfica
-
-    // Se for necessário adicionar variáveis ao objecto ChatClient, devem
-    // ser colocadas aqui
-
     private final String server;
     private final int port;
     private final ByteBuffer receiveBuffer = ByteBuffer.allocate(16384);
@@ -28,19 +19,11 @@ public class ChatClient {
     private final Charset charset = StandardCharsets.UTF_8;
     private final CharsetDecoder charsetDecoder = charset.newDecoder();
 
-
-    
-    // Método a usar para acrescentar uma string à caixa de texto
-    // * NÃO MODIFICAR *
     public void printMessage(final String message) {
         chatArea.append(message);
     }
 
-    
-    // Construtor
-    public ChatClient(String server, int port) throws IOException {
-
-        // Inicialização da interface gráfica --- * NÃO MODIFICAR *
+    public ChatClient(String server, int port) {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
@@ -54,12 +37,12 @@ public class ChatClient {
         chatBox.setEditable(true);
         chatBox.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     newMessage(chatBox.getText());
-                } catch (IOException ex) {
+                } catch (IOException e) {
                 } finally {
-                    chatBox.setText("");
+                     chatBox.setText("");
                 }
             }
         });
@@ -68,84 +51,64 @@ public class ChatClient {
                 chatBox.requestFocusInWindow();
             }
         });
-        // --- Fim da inicialização da interface gráfica
-
-        // Se for necessário adicionar código de inicialização ao
-        // construtor, deve ser colocado aqui
 
         this.server = server;
         this.port = port;
-
     }
 
-
-    // Método invocado sempre que o utilizador insere uma mensagem
-    // na caixa de entrada
     public void newMessage(String message) throws IOException {
-        // PREENCHER AQUI com código que envia a mensagem ao servidor
         sendBuffer.clear();
         sendBuffer.put(charset.encode(message));
         sendBuffer.flip();
         while (sendBuffer.hasRemaining()) {
             socketChannel.write(sendBuffer);
         }
-
-
-
     }
 
-    
-    // Método principal do objecto
     public void run() throws IOException {
-        // PREENCHER AQUI
-       socketChannel = SocketChannel.open();
+        socketChannel = SocketChannel.open();
         socketChannel.configureBlocking(false);
-       socketChannel.connect(new InetSocketAddress(server, port));
-        while(!socketChannel.finishConnect()){
+        socketChannel.connect(new InetSocketAddress(server, port));
+        while (!socketChannel.finishConnect()) {
+
         }
 
-       while (true) {
-           receiveBuffer.clear();
-           socketChannel.read(receiveBuffer);
-           receiveBuffer.flip();
-           if (receiveBuffer.limit() != 0) {
-               String message = charsetDecoder.decode(receiveBuffer).toString();
-               String messageType = message.split(" ")[0];
-               String friendlyMessage = " ";
-               if (messageType.equals("OK\n")) {
+        while (true) {
+            receiveBuffer.clear();
+            socketChannel.read(receiveBuffer);
+            receiveBuffer.flip();
+            if (receiveBuffer.limit() != 0) {
+                String message = charsetDecoder.decode(receiveBuffer).toString();
+                String[] args = message.split(" ");
+                String messageType = args[0];
+                String friendlyMessage;
+                if (messageType.equals("OK\n")) {
                     friendlyMessage = "Sucesso!\n";
-               } else if (messageType.equals("ERROR\n")) {
+                } else if (messageType.equals("ERROR\n")) {
                     friendlyMessage = "Erro!\n";
-               } else if (messageType.equals("MESSAGE")) {
-                   int messageStartIndex = messageType.length() + 2 + message.split(" ")[1].length();
-                    String newMessage = message.substring(messageStartIndex);
-                    friendlyMessage = message.split(" ")[1] + ": " + newMessage ;
-               } else if (messageType.equals("NEWNICK")) {
-                    friendlyMessage = message.split(" ")[1] + " mudou de nome para " + message.split(" ")[2];
-               } else if (messageType.equals("JOINED")) {
-                    friendlyMessage = message.split(" ")[1].replace("\n", "") + " juntou se a sala\n";
-               } else if (messageType.equals("LEFT")) {
-                    friendlyMessage = message.split(" ")[1].replace("\n", "") + " deixou a sala\n";
-               } else if (messageType.equals("PRIVATE")) {
-                   int messageStartIndex = messageType.length() + 2 + message.split(" ")[1].length();
-                   friendlyMessage = "Mensagem privada de " + message.split(" ")[1] + ": " + message.substring(messageStartIndex);
-               } else {
+                } else if (messageType.equals("MESSAGE")) {
+                    int messageStartIndex = messageType.length() + args[1].length() + 2;
+                    friendlyMessage = args[1] + ": " + message.substring(messageStartIndex);
+                } else if (messageType.equals("NEWNICK")) {
+                    friendlyMessage = args[1] + " mudou de nome para " + args[2];
+                } else if (messageType.equals("JOINED")) {
+                    friendlyMessage = args[1].replace("\n", "") + " juntou se a sala\n";
+                } else if (messageType.equals("LEFT")) {
+                    friendlyMessage = args[1].replace("\n", "") + " deixou a sala\n";
+                } else if (messageType.equals("PRIVATE")) {
+                    int messageStartIndex = messageType.length() + args[1].length() + 2;
+                    friendlyMessage = "Mensagem privada de " + args[1] + ": " + message.substring(messageStartIndex);
+                } else {
                     friendlyMessage = "Xau!\n";
-               }
+                }
 
-               printMessage(friendlyMessage);
-           }
-       }
-
-
+                printMessage(friendlyMessage);
+            }
+        }
     }
-    
 
-    // Instancia o ChatClient e arranca-o invocando o seu método run()
-    // * NÃO MODIFICAR *
     public static void main(String[] args) throws IOException {
         ChatClient client = new ChatClient(args[0], Integer.parseInt(args[1]));
         client.run();
     }
-
 }
